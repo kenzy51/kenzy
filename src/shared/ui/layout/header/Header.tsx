a73c330c38bf095233e32a588be70403c3d91b65
Nazarov Kanat <nazarovkanat7@gmail.com>
2025-09-17 16:05:46 -0400

    added big changes

--- src/shared/ui/layout/header/Header.tsx
+++ src/shared/ui/layout/header/Header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
                   {/* SOME CODE to commit*/}
                   <div className={styles.icons}>
                     <a href="https://github.com/kenzy51" target="_blank" className={styles.link}><Image alt="Github" src={github} width={30} /></a>
-                    <a href="https://www.linkedin.com/in/kanat-nazar-a8b55533a/" target="_blank" className={styles.link}><Image alt="LinkedIn" src={linkedin} width={30} /></a>
+                    <a href="https://www.linkedin.com/in/kanat-nazarov-a8b55533a/" target="_blank" className={styles.link}><Image alt="LinkedIn" src={linkedin} width={30} /></a>
                   </div>
                 </motion.div>
               )}
@@ -67,7 +67,7 @@ const Header = () => {
             </nav>
             <div className={styles.icons}>
               <a href="https://github.com/kenzy51" target="_blank" className={styles.link}><Image alt="Github" src={github} width={30} /></a>
-              <a href="https://www.linkedin.com/in/kanat-nazarov-438baa247/" target="_blank" className={styles.link}><Image alt="LinkedIn" src={linkedin} width={30} /></a>
+              <a href="https://www.linkedin.com/in/kanat-nazarov-a8b55533a/" target="_blank" className={styles.link}><Image alt="LinkedIn" src={linkedin} width={30} /></a>
             </div>
           </div>
         )}
