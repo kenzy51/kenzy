8e4c086b95c1a44449d120ea063679195408e9f8
Nazarov Kanat <nazarovkanat7@gmail.com>
2025-12-28 21:56:17 -0500

    added new meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <meta
+        name="google-site-verification"
+        content="VgDRz5M8tNsT6gLPg4D6D7UmgkNA1AM2pqjnh70aX4k"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800&display=swap"
         rel="stylesheet"
@@ -11,9 +15,11 @@ export default function Document() {
         href="https://fonts.googleapis.com/css2?family=Manrope:wght@100;200;300;400;500;600;700;800&display=swap"
         rel="stylesheet"
       />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
-
-        <link href="../output.css" rel="stylesheet"/>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      />
+      <link href="../output.css" rel="stylesheet" />
       <Head />
       <body>
         <Main />
