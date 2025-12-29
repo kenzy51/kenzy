822fde47161d749025ed1dc93f679a6330f71419
Nazarov Kanat <nazarovkanat7@gmail.com>
2025-12-28 21:47:13 -0500

    added some part

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://kenzy.vercel.app',
+  generateRobotsTxt: true, 
+  exclude: [], 
+};
