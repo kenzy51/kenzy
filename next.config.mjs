b9734ffac02ad4037e789adf316a858ea8e95fbd
Nazarov Kanat <kanatnazarov.dev@gmail.com>
2026-05-22 10:18:33 -0400

    udpated redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,38 @@
 // next.config.mjs
-import withMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
-import rehypeHighlight from 'rehype-highlight';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import withMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ['en', 'ru'],
-    defaultLocale: 'en',
+    locales: ["en", "ru"],
+    defaultLocale: "en",
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/developer',
+        source: "/",
+        destination: "/developer",
+        permanent: true,
+      },
+      {
+        source: "/musician",
+        destination: "/developer",
         permanent: true,
       },
     ];
@@ -40,7 +45,7 @@ const mdxConfig = withMDX({
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
       rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+      [rehypeAutolinkHeadings, { behavior: "wrap" }],
       rehypeHighlight,
     ],
   },
