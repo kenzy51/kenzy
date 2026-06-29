96ac21898c6e0cbf14b4ea0a8552fbdc7b9e71ab
Nazarov Kanat <kanatnazarov51@gmail.com>
2026-06-29 14:58:40 -0400

    added changes

--- src/shared/ui/slider/Slider.tsx
+++ src/shared/ui/slider/Slider.tsx
@@ -21,6 +21,9 @@ import nest from "../../../../public/images/skills/nest.png";
 import ts from "../../../../public/images/skills/ts.png";
 import redis from "../../../../public/images/skills/redis.svg";
 import next from "../../../../public/images/skills/nextjs3.webp";
+import java from "../../../../public/images/skills/java.png";
+import python from "../../../../public/images/skills/python.png";
+import net from "../../../../public/images/skills/net.png";
 import useMediaQuery from "@/shared/hooks/useMediaQuery";
 
 const imagesAll = [
@@ -38,7 +41,7 @@ const imagesAll = [
   angular,
 ];
 const imagesFrontend = [react, js, html, git, css, ts, next, angular];
-const imagesBackend = [mongo, js, nest, git, ts, node, redis];
+const imagesBackend = [mongo, js, nest, git, ts, node, redis, java, python,net];
 type Status = "frontend" | "all" | "backend";
 type SliderProps = {
   status: Status;
